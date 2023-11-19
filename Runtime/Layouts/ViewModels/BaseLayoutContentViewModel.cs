c3406f304c70ba601b4e0932e937302b28ac29b9
Lisa Vorontcova <lisavorontcova@yandex.ru>
2023-11-19 19:09:12 +0300

    Update BaseLayoutContentViewModel.cs

--- Runtime/Layouts/ViewModels/BaseLayoutContentViewModel.cs
+++ Runtime/Layouts/ViewModels/BaseLayoutContentViewModel.cs
@@ -8,7 +8,7 @@ namespace UIFlow.Runtime.Layouts.ViewModels
         private static readonly string m_ShowCommandPropertyName = nameof(ShowCommand);
         private static readonly string m_HideCommandPropertyName = nameof(HideCommand);
         
-        private bool m_Showed;
+        private bool m_IsShowed;
 
         private ICommand m_ShowCommand;
         private ICommand m_HideCommand;
@@ -24,22 +24,24 @@ namespace UIFlow.Runtime.Layouts.ViewModels
             get => m_HideCommand;
             set => SetProperty(ref m_HideCommand, value, m_HideCommandPropertyName);
         }
+
+        public bool IsShowed => m_IsShowed;
         
         public event Action<BaseLayoutContentViewModel> Showed;
         public event Action<BaseLayoutContentViewModel> Hidden;
 
         protected BaseLayoutContentViewModel()
         {
-            m_ShowCommand = new Command(_ => !m_Showed,_ => Show());
-            m_HideCommand = new Command(_ => m_Showed, _ => Hide());
+            m_ShowCommand = new Command(_ => !m_IsShowed,_ => Show());
+            m_HideCommand = new Command(_ => m_IsShowed, _ => Hide());
         }
         
         private void Show()
         {
-            if(m_Showed)
+            if(m_IsShowed)
                 return;
             
-            m_Showed = true;
+            m_IsShowed = true;
             
             OnBeforeShow();
             Showed?.Invoke(this);
@@ -48,10 +50,10 @@ namespace UIFlow.Runtime.Layouts.ViewModels
 
         private void Hide()
         {
-            if(!m_Showed)
+            if(!m_IsShowed)
                 return;
             
-            m_Showed = false;
+            m_IsShowed = false;
             
             OnBeforeHide();
             Hidden?.Invoke(this);
